f7f5212ef03df01aaaebb8a0aa85988128439c41
tyrone <tyronekisienya01@gmail.com>
2023-06-15 13:19:07 -0700

    UnixStd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdion.h>
+
+#include <unistd.h>
+
+/**
+ * main - prints quote
+ *
+ * Return: 1
+ */
+
+int main(void)
+
+	{
+		write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+		return (1);
