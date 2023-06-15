a88a5a01a8f9f5c7e585c005a01173a5a6d1c8bd
tyrone <tyronekisienya01@gmail.com>
2023-06-15 12:03:47 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main function
+ * returns 0 when correct
+ */
+
 int main(void)
+
 	{
-		puts("\"Programming is like building a multilingual puzzle\n");
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 	}
