a69a273f180cc24c762c03941a9a7ff291dd57ad
tyrone <tyronekisienya01@gmail.com>
2023-06-16 11:05:58 -0700

    Prints Alpha_alpha

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,11 +13,11 @@ int main(void)
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
-		for (c = 'A'; c <= 'Z'; c++)
-		{
-			putchar(c);
-		}
 	}
-	putchar('\n');
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar("\n");
 	return (0);
 }
