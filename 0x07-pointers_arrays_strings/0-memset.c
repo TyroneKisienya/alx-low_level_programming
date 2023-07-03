fac55138a5ad7ec5cdf1acc6ad727ddca7639f4b
tyrone <tyronekisienya01@gmail.com>
2023-07-03 04:53:32 -0700

    Constant refill

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -19,3 +19,19 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 	return (s);
 }
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[98] = {0x00};
+
+    simple_print_buffer(buffer, 98);
+    _memset(buffer, 0x01, 95);
+    printf("-------------------------------------------------\n");
+    simple_print_buffer(buffer, 98);
+    return (0);
+}
