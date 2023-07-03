3e06c8cf70390d00c96ed779a7462bf652c4adff
tyrone <tyronekisienya01@gmail.com>
2023-07-03 05:33:28 -0700

    header

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,15 @@
-#ifndef HEADER_FILE_H
-#define HEADER_FILE_H
+#ifndef MAIN_H_
+#define MAIN_H_
 
-#include <cstring>
-
-char *_memset(char *s, int c, size_t n);
+char *_memset(char *s, char b, unsigned int n);
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
-
