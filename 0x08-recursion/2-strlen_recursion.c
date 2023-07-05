8a381a757aea31cbd494330c5d31d2b3e3e975a0
tyrone <tyronekisienya01@gmail.com>
2023-07-05 01:20:42 -0700

    counter

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - strlen with recursion method
+ *
+ * @s: the string
+ *
+ * Return: integer length of s
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (1 + _strlen_recursion(s));
+}
