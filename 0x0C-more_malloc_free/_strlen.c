0218d910e23d529c4c8ea02354c2a313f16d11a8
tyrone <tyronekisienya01@gmail.com>
2023-07-13 13:49:46 -0700

    cutter

---
+++ 0x0C-more_malloc_free/_strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen -return string length
+ *
+ * @string: the string
+ *
+ * Return: int
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
