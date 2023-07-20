b770f6617a141bc9d46c7ffe4330414f552d9b08
tyrone <tyronekisienya01@gmail.com>
2023-07-20 08:28:29 -0700

    more functs

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+vari_funct
---
+++ 0x10-variadic_functions/printc.c
@@ -0,0 +1,11 @@
+#include "variadic_functions.h"
+
+/**
+ * printc - print char
+ *
+ * @vprint: the char
+ */
+void printc(va_list vprint)
+{
+	printf("%c", va_arg(vprint, int));
+}
