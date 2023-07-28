03cc33b9abe012e7caeeccd4d1cd0e9788e4cf06
SKai <tyronekisienya01@gmail.com>
2023-07-29 02:28:46 +0300

    prints the alphabets 10ts

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,16 @@
  */
 void print_alphabet_x10(void)
 {
+	char c;
 	int i;
+
 	while (i < 10)
 	{
-		for (i = 'a'; i <= 'z'; i++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar(i);
+			_putchar(c);
 		}
 		_putchar('\n');
+		i++
 	}
 }
