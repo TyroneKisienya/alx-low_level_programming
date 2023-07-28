7f0f3e37caa06d767044b590cfa77b0e120d4499
SKai <tyronekisienya01@gmail.com>
2023-07-29 01:54:35 +0300

    prints alphabets using putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main -Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
