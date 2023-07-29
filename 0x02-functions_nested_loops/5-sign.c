a1b461680a1dfb80859d508ef207c596d5c18071
SKai <tyronekisienya01@gmail.com>
2023-07-29 03:38:50 +0300

    prints the sign of a no

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign -entry
+ *
+ * @n: prints whether zero + or -
+ *
+ * Return: 1 if + 0 if zero -1 if -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+, ");
+		return (1);
+		if (n == 0)
+		{
+			printf("0, ");
+			return (0);
+		}
+	}
+		else
+		{
+			printf("-, ");
+			return (-1);
+		}
+}
