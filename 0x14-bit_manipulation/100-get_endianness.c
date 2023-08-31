af6c6b25a407083ec28850159eebabcc33b1bc09
Skai <Tyronekisienya01@gmail.com>
2023-09-01 02:00:05 +0300

    seeks endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,15 @@
-#
+#include "main.h"
+
+/**
+ * get_endianness - check for endianness
+ *
+ * Return: 0 big else 1 little
+ */
+int get_endianness(void)
+{
+	int m = 1;
+	char *seek;
+
+	seek = (char *)&m;
+	return ((int)seek[0]);
+}
