94a0106938f6035bc7e584f677c37a7bf8619038
Skai <Tyronekisienya01@gmail.com>
2023-08-31 13:27:39 +0300

    Bit conv

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converter
+ *
+ * @b: var
+ *
+ * Return: num
+ */
+
+unsigned int binary_to_uint(const char *b);
+{
+	unsigned int num = 0;
+
+	if (b == NULL)
+		return (0);
+	return (num);
+}
