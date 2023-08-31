9ddea483e4873f3c959c2fa2548201e3edc8181f
Skai <Tyronekisienya01@gmail.com>
2023-09-01 01:04:02 +0300

    ret val at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - ret val at point
+ *
+ * @n: val
+ *
+ * @index: giver
+ *
+ * Return: success 0 else 1, -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int m;
+	unsigned int max;
+
+	max = (sizeof(unsigned long int) * 8);
+	if (index > max)
+		return (-1);
+	m = ((n >> index) & 1);
+	return (b);
+}
