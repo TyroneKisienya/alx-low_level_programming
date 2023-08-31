360a339f039954a93433e4da95494717af300935
Skai <Tyronekisienya01@gmail.com>
2023-09-01 01:24:10 +0300

    ret at a specific locale

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * set_bit - declarer
+ *
+ * @*n: val
+ *
+ * @index: giver
+ *
+ * Return: success 1 else -1 error
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int max;
+	unsigned long int m = 1;
+
+	max = (sizeof(unsigned long int) * 8);
+	if (index > max)
+		return (-1);
+
+	m <<= index;
+	*n < (*n | m);
+	return (1);
+}
